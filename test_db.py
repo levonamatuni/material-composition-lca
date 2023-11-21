31d47f497845a2e8c684449d47495b2469e9c3f0
Amatuni L <levonamatuni@gmail.com>
2023-11-21 15:53:30 +0100

    Update test_db.py
    Test_db is now adjusted to:
    - work with the most recent version of bw2 (Nov 2023)
    - include comments for practitioners

--- test_db.py
+++ test_db.py
@@ -9,23 +9,28 @@ Created on Wed Jul 17 18:52:09 2019
 @author: amatunilt
 """
 
-from brightway2 import *
+#from brightway2 import *
+import brightway2 as bw
 import numpy as np
 
-projects.set_current("test")
+#CONST
+PROD = 'laptop'
+
+
+bw.projects.set_current("default")
 #bw2setup()
 
-db = Database("db")
+db = bw.Database("db")
 list_dissip = {"factory"}
 
 #dictionary relates meaningful materials with the corresponding/contributing activities (their keys)
 materials_dict = {
-        "plastic":
-            {"PET": [('db', '243c83e4e16546f58c2f301351a0397d')],
-             "PP": [('db', 'fcc4443e7ee64e88b89c9c4d93404d8a')],
+        "plastics":
+            {"PET": [],
+             "PP": [],
             },
         "metals":
-            {"copper": [('db', 'e454d24c657340cdb9e20733b8c97a62')]
+            {"copper": [('db', '9d4a7be1e15944dcb936132aea869546')]
             }
 }
 
@@ -33,28 +38,42 @@ def activity_by_name(name, db):
     candidates = [x for x in db if name in x['name']]
     return candidates[0]
 
+#obtain activity object given its key (tuple(db, key))
 def activity_by_key(key): # tuple(db, key) -> activity (dataset)
     return db.get(key[1])
 
+#list all intermediate (technosphere) flows (activities) in the resulting supply array based on LCI analysis
 def list_techno_inventory(lca):
+    print("Supply array: ")
     for k in lca.activity_dict:
         print(activity_by_key(k)["name"], ": ", lca.supply_array[lca.activity_dict[k]])
     print('\n')
     
+#aggregate and print material fllows from the resulting supply_array based on the predefined 'material dictionary'
 def composition(materials_dict, lca): #presents product composition from the exisitng lca object based on the metrials dicionary
+    print("Material composition (based on supply array): ")
     for material_group in materials_dict:
         gr_sum = 0
         for material in materials_dict[material_group]:
             mat_sum = 0
             for act_key in materials_dict[material_group][material]:
                 mat_sum += lca.supply_array[lca.activity_dict[act_key]]
-            print('\t', material, " = ", mat_sum)
+            print('\t', material, " : ", mat_sum, ' kg')
             gr_sum += mat_sum
-        print(material_group, " = ", gr_sum)
+        print(material_group, "in total : ", gr_sum, ' kg')
     print('\n')
+    
+#print technospheere matrix (A)
+def print_techno_matrix(lca):
+    matrix_size = np.shape(lca.technosphere_matrix)[0]
+    for i in range(matrix_size):
+        print("\n")
+        for j in range(matrix_size):
+            print(lca.technosphere_matrix[i, j], end = ' ')         
+        
+#MAIN code
 
-
-act = activity_by_name('PC', db)
+act = activity_by_name(PROD, db)
 
 #assigning 'dissipation' parameter
 for exc in act.technosphere():
@@ -62,34 +81,32 @@ for exc in act.technosphere():
     exc['dissip'] = 1 if exc_name in list_dissip else 0
     exc.save()
     
-#LCI
+#LCI prior to changing the technosphere matrix (filtering out non-incorporated flows)
+print("BEFORE filtering:\n")
 functional_unit = {act: 1}
-method_key = [x for x in methods if "non-renewable resources, copper" in x][0]    
-lca = LCA(functional_unit, method_key)
+method_key = [x for x in bw.methods if "copper" in x][0] #pick the environmental pressure (extracted material/resource from the list of impact methods)
+lca = bw.LCA(functional_unit, method_key) 
 
 lca.lci()
-print("LCI (before): ", list_techno_inventory(lca))
-print(composition(materials_dict, lca))
 
+list_techno_inventory(lca)
+composition(materials_dict, lca)
 
+#LCI afyer changing the technosphere matrix (filtering out non-incorporated flows)
+print("AFTER filtering:\n")
+
+#removing the non-incorporated inputs from the technosphere matrix 
 for exc in act.technosphere():
     if  exc['dissip']:
         row = lca.activity_dict[exc["input"]]
         col = lca.activity_dict[act.key]
         lca.technosphere_matrix[row, col] = 0
 
-
-#output techn. matrix
-#for i in range(7):
-#    print("-")
-#    for j in range(7):
-#        print(lca.technosphere_matrix[i, j], end = ' ')
-
 lca.lci_calculation()
 lca.lcia()
 
-print("LCI (after): ", list_techno_inventory(lca))
-print(method_key, ' : ', lca.score)
-print(composition(materials_dict, lca))
+list_techno_inventory(lca)
+print("Impact method selected by the user: ", method_key, ' : ', lca.score, '\n')
+composition(materials_dict, lca)
 
 
